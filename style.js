2f62704905415a326c61c05b8d57d75e58b41886
NNUTSI <89682164+NNUTSI@users.noreply.github.com>
2022-02-15 13:50:45 +0400

    again

--- style.js
+++ style.js
@@ -1,9 +1,9 @@
 'use strict';
 
 
-const secretNumber = Math.trunc (Math.random () * 20) + 1;
+let secretNumber = Math.trunc (Math.random () * 20) + 1;
 let score =20;
-document.querySelector('.number').textContent = secretNumber;
+
  
 document.querySelector('.check').addEventListener('click' , function () {
    const guess = Number (document.querySelector('.guess').value);
@@ -16,9 +16,11 @@ document.querySelector('.check').addEventListener('click' , function () {
 // when player wins 
   }else if(guess === secretNumber){
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
   document.querySelector ('body').style.backgroundColor = '#60b347';
   document.querySelector ('.number').style.width = '30rem';
+  
 
 // when guess is too high
   } else if (guess > secretNumber){
@@ -43,3 +45,16 @@ document.querySelector('.check').addEventListener('click' , function () {
       }
   }
 });
+document.querySelector('.again').addEventListener('click', function(){
+   score = 20;
+   secretNumber = Math.trunc (Math.random () * 20) + 1;
+
+   document.querySelector('.message').textContent ='Start guessing...';
+   document.querySelector('.score').textContent = score;
+   document.querySelector('.number').textContent ='?';
+   document.querySelector('.guess').value = '';
+
+   document.querySelector ('body').style.backgroundColor = '#151615';
+   document.querySelector ('.number').style.width = '15rem';
+
+} );
