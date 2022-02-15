334e81caadbaa686f5d38b15f7e5022eddad0ec1
NNUTSI <89682164+NNUTSI@users.noreply.github.com>
2022-02-16 01:08:41 +0400

    highscore

--- style.js
+++ style.js
@@ -3,6 +3,7 @@
 
 let secretNumber = Math.trunc (Math.random () * 20) + 1;
 let score =20;
+let highscore = 0;
 
  
 document.querySelector('.check').addEventListener('click' , function () {
@@ -21,6 +22,10 @@ document.querySelector('.check').addEventListener('click' , function () {
   document.querySelector ('body').style.backgroundColor = '#60b347';
   document.querySelector ('.number').style.width = '30rem';
   
+  if (score > highscore) {
+    highscore = score;
+    document.querySelector ('.highscore').textContent = highscore;
+  };
 
 // when guess is too high
   } else if (guess > secretNumber){
